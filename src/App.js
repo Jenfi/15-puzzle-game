e349f4065dfe886f9e512f8372ccd4dbc55f3a37
Jenfi <jennifer_fick@hotmail.com>
2020-03-24 17:29:56 +0100

    set up button function

--- src/App.js
+++ src/App.js
@@ -24,6 +24,23 @@ export const App = () => {
     { number: 0 }, //index 14
   ]
 
+  const newGame = [
+    { number: 1 }, //index 0
+    { number: 2 }, //index 1
+    { number: 3 }, //index 2
+    { number: 4 }, //index 3
+    { number: 5 }, //index 4
+    { number: 6 }, //index 5
+    { number: 7 }, //index 6
+    { number: 8 }, //index 7
+    { number: 9 }, //index 8
+    { number: 10 }, //index 9
+    { number: 11 }, //index 10
+    { number: 12 }, //index 11
+    { number: 13 }, //index 12
+    { number: 14 }, //index 13
+    { number: 0 }, //index 14
+  ]
   // Returns randomized order of game-array
   const randomizer = (arr) => {
     let i, j, temp;
@@ -36,15 +53,16 @@ export const App = () => {
     return arr;
   }
 
-  //Button that that randomly shuffles the order of the  objects in the game array
+  const [bricks, setBricks] = useState(randomizer(game))
+
+  //Button that randomly shuffles the order of the  objects in the game array
   const handleShuffle = () => {
     return randomizer(game)
   }
 
-  handleShuffle()
 
 
-  const handleSlide = () => {
+  const handleMove = () => {
     // a function that handles the move of the numbers. Splice or similare? Many if-statements...?
   }
 
@@ -59,7 +77,8 @@ export const App = () => {
           <div
             className={brick.number !== 0
               ? "game-brick"
-              : "no-brick"}>
+              : "no-brick"}
+            key={brick.number}>
             {brick.number !== 0
               ? <p>{brick.number}</p>
               : <p></p>}
@@ -69,7 +88,7 @@ export const App = () => {
       <button
         className="shuffle-button"
         type="button"
-        onClick={handleShuffle()}>
+        onClick={() => setBricks(randomizer(newGame))}>
         SLUMPA
         </button>
     </main >
