d3861d836a72cd9a3ed85d3e6cb384d96bb26826
Jenfi <jennifer_fick@hotmail.com>
2020-03-26 14:59:55 +0100

    if(check if up)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './styling/game.css'
 export const App = () => {
   const [won, setWon] = useState(false)
   // const rowArray = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, null]]
-  const rowArray = [[1, 3], [2, null]]
+  const rowArray = [[1, 3], [2, null], [4, 5]]
 
 
   // const [moveUp, setMoveUp] = useState(true)
@@ -69,32 +69,49 @@ export const App = () => {
           <div key={rowIndex} className="row">
             {row.map((brickValue, columnIndex) => {
 
-              //Checking if brick can move up (if rowIndex = 0, or if brickValue above is not null. --> Hur får jag ut brickValue ovanför? )
-              if (rowIndex === 0 || rowIndex - 1 && columnIndex && brickValue !== null) {
-                console.log(`${brickValue} - this brick cannot move up`)
-              } else {
+
+              //Checking if brick can move up (if rowIndex = 0, or if brickValue above is not null. )
+              if (rowIndex !== 0 && rowArray[rowIndex - 1][columnIndex] === null) {
+
+                // if (rowIndex === 0) {
                 console.log(`${brickValue} - this brick can move up`)
+
+                // } else if (rowIndex !== 0 && row[rowIndex - 1] && brickValue !== null) {
+                // console.log(`${brickValue} - this brick cannot move up`)
+              } else {
+                console.log(`${brickValue} - this brick cannot move up`)
+
               }
 
               //Checking if brick can move down (if rowIndex = number of elements in array, or if brickValue below is not null)
-              if (rowIndex === rowArray.length - 1 || rowIndex + 1 && brickValue !== null) {
-                console.log(`${brickValue} - this brick cannot move down`)
-              } else {
-                console.log(`${brickValue} - this brick can move down`)
+              if (rowIndex === rowArray.length - 1 || rowIndex !== rowArray.length - 1 && rowArray[rowIndex + 1] && columnIndex[columnIndex] && brickValue !== null) {
+
+                // if (rowIndex === rowArray.length - 1) {
+                // console.log(`${brickValue} - this brick cannot move down`)
+                // console.log(`${brickValue} - columnIndex: ${columnIndex}`)
+                // console.log(`${brickValue} - rowIndex: ${rowIndex}`)
+
+
+                // } else if (rowIndex !== rowArray.length - 1 && rowArray[rowIndex + 1] && columnIndex[columnIndex] && brickValue !== null) {
+                //   console.log(`${brickValue} - this brick cannot move down`)
+              }
+              else {
+                // console.log(`${brickValue} - this brick can move down`)
+
               }
 
               //Checking if brick can move to the right
               if (columnIndex === row.length - 1) {
-                console.log(`${brickValue} - this brick cannot move to the right`)
+                // console.log(`${brickValue} - this brick cannot move to the right`)
               } else {
-                console.log(`${brickValue} - this brick can move to the right`)
+                // console.log(`${brickValue} - this brick can move to the right`)
               }
 
               //Checking if brick can move to the left
               if (columnIndex !== 0) {
-                console.log(`${brickValue} - this brick can move to the left`)
+                // console.log(`${brickValue} - this brick can move to the left`)
               } else {
-                console.log(`${brickValue} - this brick cannot move to the left`)
+                // console.log(`${brickValue} - this brick cannot move to the left`)
               }
 
 
@@ -103,7 +120,8 @@ export const App = () => {
                   key={`${rowIndex}-${columnIndex}`}
                   className={brickValue !== null
                     ? "game-brick"
-                    : "null-brick"} onClick="">
+                    : "null-brick"}
+                  onClick={handleMove}>
                   {brickValue}
                 </div>
               )
