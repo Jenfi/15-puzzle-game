e582e5b6da80710f3275021403a66597e51c5b5f
Jenfi <jennifer_fick@hotmail.com>
2020-03-27 15:24:56 +0100

    Cleaned some code and updated readme

--- README.md
+++ README.md
@@ -1,19 +1,27 @@
-# Technigo React Starter App
+# 15 Game puzzle
 
-This app has been generated using `create-react-app`, then cleaned up a little so we have a project with just the elements we need to get started.
+Den här appen har genererats med hjälp av boilerplate  `Technigo-react-starter`
 
-Add components in the `src` folder and use them in `src/app.js` to get started.
+## Byggnad
 
-## Usage
-
-1. On GitHub, click the 'use this template' button to create a new repo on your account
-1. Clone the new repo to your computer
 1. Install required dependencies with npm: `npm install`
 1. Start the project: `npm start`
 
-Alternatively, if you don't want a new git repo on your account;
+## Arbete med projektet
+- Jag la ner relativt mycket tid på det här projektet, framför allt för att komma fram till den grundläggande logiken. Totalt la jag ner ca två arbetsdagar. Det har varit ett väldigt roligt projekt att arbeta med.
 
-1. Download & extract the [latest release](https://github.com/Technigo/react-starter/releases/latest) from GitHub
-1. Open the directory in the terminal: `cd /path/to/react-starter`
-1. Install required dependencies with npm: `npm install`
-1. Start the project: `npm start`
+* Processen:
+Jag började med att sätta mig in i projektet och säkerställa att jag förstod hur slutresultatet skulle se ut. Därefter bröt jag ner projektet i mindre delar, där jag följde denna ordning;
+
+1: Satte upp modellen för spelet i form av en 2D-array
+2: Satte upp logiken för hur brickorna kan/inte kan flytta sig.
+3: Satte upp funktionen för hur brickorna kan/inte kan flytta sig och förankrade det till brickorna i spelet.
+4: Satte upp jämförelsen mellan spelande array och vinnande array för att kunna visa användaren om den klarat spelet eller inte.
+5: Satte upp shuffle-funktionen
+
+* Brister:
+För att göra koden lite renare skulle jag kunna bryta ut olika komponenter, bl.a. "Slumpa"-knappen och koden som hanterar om användaren har vunnit spelet.
+
+
+* Mest nöjd med:
+Med det här projektet är jag mest nöjd med logiken bakom funktionen canMove. Här lade jag ner mest tid i min kod för att fundera ut logiken och få koden att fungera. Jag tycker även att koden är lättläst.
--- src/App.js
+++ src/App.js
@@ -6,41 +6,10 @@ export const App = () => {
   const [rowArray, setRowArray] = useState([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, null, 14]])
   const winningArray = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, null]]
 
-  // GLÖM EJ INTE FIREWORKS!!!
-  // ANIMATION?
-
   // Compare rowArray with winningArray to see if the game is won
-  // const isGameWon = (currentRowIndex, currentColumnIndex) => {
-  //   if (rowArray[currentRowIndex][currentColumnIndex] !== winningArray[currentRowIndex][currentColumnIndex] && rowArray !== winningArray) {
-  //     // if (rowArray[currentRowIndex] !== winningArray[currentRowIndex] && rowArray[currentColumnIndex] !== winningArray[currentColumnIndex]) {
-  //     console.log('not won')
-  //     console.log(rowArray)
-  //     console.log(rowArray[currentRowIndex][currentColumnIndex])
-
-  //     return false;
-  //   } else if (rowArray === winningArray) {
-  //     console.log('winning')
-  //     console.log(rowArray[currentRowIndex][currentColumnIndex])
-
-  //     console.log(rowArray)
-  //     return true
-  //   }
-  // }
-
-  // const isGameWon = (row) => {
-  //   for (var i = 0; rowArray.length < i; i++) {
-  //     for (var j = 0; row.length < j; j++) {
-  //       if (rowArray[i][j] !== winningArray[i][j]) {
-  //         return false
-  //       }
-  //     }
-  //   }
-  //   return true
-  // }
-
   const isGameWon = () => {
-    for (var i = 0; rowArray.length - 1 >= i; i++) {
-      for (var e = 0; rowArray[i].length - 1 >= e; e++) {
+    for (let i = 0; rowArray.length - 1 >= i; i++) {
+      for (let e = 0; rowArray[i].length - 1 >= e; e++) {
         if (rowArray[i][e] !== winningArray[i][e]) {
           return false;
         }
@@ -49,32 +18,15 @@ export const App = () => {
     return true
   }
 
-  // // Returns randomized order of game-array
-  // const shuffleArray = () => {
-  //   //   let currentIndex = array.lenght, temporaryValue, randomIndex;
-  //   for (var i = 0; i < rowArray.length; i++) {
-  //     for (var j = 0; j < rowArray[i].length; j++) {
-  //       var i1 = Math.floor(Math.random() * (rowArray.length))
-  //       var j1 = Math.floor(Math.random() * (rowArray.length))
-
-  //       var temp = rowArray[i][j]
-  //       rowArray[i][j] = rowArray[i1][j1]
-  //       rowArray[i1][j1] = temp
-  //     }
-  //   }
-  //   // return rowArray
-  //   setRowArray(rowArray)
-  // }
-
   const shuffleArray = () => {
-    let newRowArray = rowArray.slice(0);
+    // Copy array and shuffle the copy to avoid rowArray being shuffled when not asked for
+    const newRowArray = rowArray.slice(0);
 
-    for (var i = 0; i < newRowArray.length; i++) {
-      for (var j = 0; j < newRowArray[i].length; j++) {
-        var i1 = Math.floor(Math.random() * (newRowArray.length))
-        var j1 = Math.floor(Math.random() * (newRowArray.length))
-
-        var temp = newRowArray[i][j]
+    for (let i = 0; i < newRowArray.length; i++) {
+      for (let j = 0; j < newRowArray[i].length; j++) {
+        const i1 = Math.floor(Math.random() * (newRowArray.length))
+        const j1 = Math.floor(Math.random() * (newRowArray.length))
+        const temp = newRowArray[i][j]
         newRowArray[i][j] = newRowArray[i1][j1]
         newRowArray[i1][j1] = temp
       }
@@ -82,36 +34,27 @@ export const App = () => {
     setRowArray(newRowArray);
   }
 
-
-  // Handles state of bricks, with randomizer(game)
-  // as initial to return a random order of bricks from start
-  // const [bricks, setBricks] = useState(randomizer(rowArray))
-  // Uppdatera denna för att kunna köra en randomized?
-
+  // Check if the brick can move
   const canMove = (currentRowIndex, currentColumnIndex, brickValue, row) => {
     // make a copy of rowArray to update state of rowArray with the copy
-    let newRowArray = rowArray.slice(0);
+    const newRowArray = rowArray.slice(0);
 
     // Checking if brick can move up (if rowIndex != 0, or if brickValue above is not null. )
-    if (currentRowIndex !== 0
-      && rowArray[currentRowIndex - 1][currentColumnIndex] === null) {
+    if (currentRowIndex !== 0 && rowArray[currentRowIndex - 1][currentColumnIndex] === null) {
       newRowArray[currentRowIndex - 1][currentColumnIndex] = brickValue;
       newRowArray[currentRowIndex][currentColumnIndex] = null;
       setRowArray(newRowArray)
-    } else if (currentRowIndex !== rowArray.length - 1
-      && rowArray[currentRowIndex + 1][currentColumnIndex] === null) {
+    } else if (currentRowIndex !== rowArray.length - 1 && rowArray[currentRowIndex + 1][currentColumnIndex] === null) {
       // Checking if brick can move down (rowIndex = number of elements in array, or if brickValue below is not null)
       newRowArray[currentRowIndex + 1][currentColumnIndex] = brickValue;
       newRowArray[currentRowIndex][currentColumnIndex] = null;
       setRowArray(newRowArray)
-    } else if (currentColumnIndex !== row.length - 1
-      && rowArray[currentRowIndex][currentColumnIndex + 1] === null) {
+    } else if (currentColumnIndex !== row.length - 1 && rowArray[currentRowIndex][currentColumnIndex + 1] === null) {
       // Checking if brick can move to the right
       newRowArray[currentRowIndex][currentColumnIndex + 1] = brickValue;
       newRowArray[currentRowIndex][currentColumnIndex] = null;
       setRowArray(newRowArray)
-    } else if (currentColumnIndex !== 0
-      && rowArray[currentRowIndex][currentColumnIndex - 1] === null) {
+    } else if (currentColumnIndex !== 0 && rowArray[currentRowIndex][currentColumnIndex - 1] === null) {
       // Checking if brick can move to the left
       newRowArray[currentRowIndex][currentColumnIndex - 1] = brickValue;
       newRowArray[currentRowIndex][currentColumnIndex] = null;
@@ -119,8 +62,6 @@ export const App = () => {
     }
     setWon(isGameWon(currentRowIndex, currentColumnIndex));
   }
-
-  // console.log(shuffleArray())
 
   return (
     <article className="main-container">
@@ -132,18 +73,17 @@ export const App = () => {
       <div className="game-board">
         {rowArray.map((row, currentRowIndex) => (
           <div key={currentRowIndex} className="row">
-            {row.map((brickValue, currentColumnIndex) => {
-
-              return (
-                <div
-                  key={`${currentRowIndex}-${currentColumnIndex}`}
-                  className={brickValue !== null
-                    ? "game-brick"
-                    : "null-brick"}
-                  onClick={() => canMove(currentRowIndex, currentColumnIndex, brickValue, row)}>
-                  {brickValue}
-                </div>)
-            })}
+            {row.map((brickValue, currentColumnIndex) => (
+              <div
+                key={`${currentRowIndex}-${currentColumnIndex}`}
+                className={brickValue !== null
+                  ? 'game-brick'
+                  : 'null-brick'}
+                role="button"
+                onClick={() => canMove(currentRowIndex, currentColumnIndex, brickValue, row)}>
+                {brickValue}
+              </div>
+            ))}
           </div>
         ))}
       </div>
