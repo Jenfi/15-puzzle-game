16964dd7f8833d6afcecd90728a7c40d8c675ed4
Jenfi <jennifer_fick@hotmail.com>
2020-03-27 11:15:39 +0100

    added div for winning h1

--- src/App.js
+++ src/App.js
@@ -12,9 +12,16 @@ export const App = () => {
   // Compare rowArray with winningArray to see if the game is won
   const isGameWon = (currentRowIndex, currentColumnIndex) => {
     if (rowArray[currentRowIndex][currentColumnIndex] !== winningArray[currentRowIndex][currentColumnIndex]) {
+      console.log('not won')
+      console.log(rowArray)
       return false;
     } else {
+      console.log('winning')
+      console.log(rowArray[currentRowIndex][currentColumnIndex])
+
+      console.log(rowArray)
       return true
+
     }
   }
 
@@ -59,52 +66,51 @@ export const App = () => {
   // }
   // console.log(shuffleRow(row))
 
-
-  // Handles state of bricks, with randomizer(game) as initial to return a random order of bricks from start
-  // const [bricks, setBricks] = useState(randomizer(rowArray)) // Uppdatera denna för att kunna köra en randomized?
-
+  // Handles state of bricks, with randomizer(game)
+  // as initial to return a random order of bricks from start
+  // const [bricks, setBricks] = useState(randomizer(rowArray))
+  // Uppdatera denna för att kunna köra en randomized?
 
   const canMove = (currentRowIndex, currentColumnIndex, brickValue, row) => {
-
-    //make a copy of rowArray to update state of rowArray with the copy
+    // make a copy of rowArray to update state of rowArray with the copy
     let newRowArray = rowArray.slice(0);
 
-    //Checking if brick can move up (if rowIndex != 0, or if brickValue above is not null. )
-    if (currentRowIndex !== 0 && rowArray[currentRowIndex - 1][currentColumnIndex] === null) {
+    // Checking if brick can move up (if rowIndex != 0, or if brickValue above is not null. )
+    if (currentRowIndex !== 0
+      && rowArray[currentRowIndex - 1][currentColumnIndex] === null) {
       newRowArray[currentRowIndex - 1][currentColumnIndex] = brickValue;
       newRowArray[currentRowIndex][currentColumnIndex] = null;
       setRowArray(newRowArray)
-    } else if (currentRowIndex !== rowArray.length - 1 && rowArray[currentRowIndex + 1][currentColumnIndex] === null) {
-      //Checking if brick can move down (if rowIndex = number of elements in array, or if brickValue below is not null)
+    } else if (currentRowIndex !== rowArray.length - 1
+      && rowArray[currentRowIndex + 1][currentColumnIndex] === null) {
+      // Checking if brick can move down (rowIndex = number of elements in array, or if brickValue below is not null)
       newRowArray[currentRowIndex + 1][currentColumnIndex] = brickValue;
       newRowArray[currentRowIndex][currentColumnIndex] = null;
       setRowArray(newRowArray)
-
-    } else if (currentColumnIndex !== row.length - 1 && rowArray[currentRowIndex][currentColumnIndex + 1] === null) {
-      //Checking if brick can move to the right
+    } else if (currentColumnIndex !== row.length - 1
+      && rowArray[currentRowIndex][currentColumnIndex + 1] === null) {
+      // Checking if brick can move to the right
       newRowArray[currentRowIndex][currentColumnIndex + 1] = brickValue;
       newRowArray[currentRowIndex][currentColumnIndex] = null;
       setRowArray(newRowArray)
-    } else if (currentColumnIndex !== 0 && rowArray[currentRowIndex][currentColumnIndex - 1] === null) {
-      //Checking if brick can move to the left
+    } else if (currentColumnIndex !== 0
+      && rowArray[currentRowIndex][currentColumnIndex - 1] === null) {
+      // Checking if brick can move to the left
       newRowArray[currentRowIndex][currentColumnIndex - 1] = brickValue;
       newRowArray[currentRowIndex][currentColumnIndex] = null;
       setRowArray(newRowArray)
-
     }
     setWon(isGameWon(currentRowIndex, currentColumnIndex));
   }
 
   return (
     <article className="main-container">
-
-      <div className="game-board">
+      <div className="winning-container">
         {won && (
-          <>
-            {/* <div className="winning-overlay"></div> */}
-            <h1 className="winning-title">Bra jobbat, du klarade det!</h1>
-          </>
+          <h1 className="winning-title">Bra jobbat, du klarade det!</h1>
         )}
+      </div>
+      <div className="game-board">
         {rowArray.map((row, currentRowIndex) => (
           <div key={currentRowIndex} className="row">
             {row.map((brickValue, currentColumnIndex) => {
